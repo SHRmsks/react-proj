fa9e70de2d76f425065997dcdf0e40b20257e4ea
SHRmsks <157053605+SHRmsks@users.noreply.github.com>
2024-05-24 23:01:13 -0400

    first commit

---
+++ aboutme/src/App.js
@@ -0,0 +1,12 @@
+import './tailwind.css';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text-blue-500">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
