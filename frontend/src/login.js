64173e20136c3e5238d292196a7f0228dab1b78e
SHRmsks <157053605+SHRmsks@users.noreply.github.com>
2024-06-14 23:34:20 -0400

    login pages\

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,38 +1,35 @@
 import "./tailwind.css";
-import TextField from "@mui/material/TextField/index.js";
-import Autocomplete from "@mui/material/Autocomplete/index.js";
-import { useState } from "react";
-import Button from "@mui/material/Button/index.js";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import Button from "@mui/material/Button";
+
+
+function LoginPage() {
 
 const suggestedName = [{ label: "Haoran" }, { label: "SHRmsks" }];
-function LoginPage() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const namehandeler = (e) => {
-    setUsername(e.target.value);
-  };
-  const passhandeler = (e) => {
-    setPassword(e.target.value);
-  };
-  const autohandler = (event, newValue) => {
-    if (newValue !== null) {
-      setUsername(newValue);
-    } else {
-      setUsername("");
-    }
-  };
-
   return (
     <div>
       <form>
+        <div className="grid max-grid-cols-1 gap-y-[1vh]  2xl:max-w-[40%] xl:max-w-[50%] lg:max-w-[60%]">
+        <div className="flex items-center flex-nowrap gap-x-[10px]">
         <label htmlFor="username">Username: </label>
-        <Autocomplete freeSolo options={suggestedName.map((option) => option.label)} value={username} type="text" label="your username" id="username" onChange={autohandler} renderInput={({ params }) => <TextField {...params} label="your username" onChange={namehandeler} />} />
-
+        <Autocomplete freeSolo options={suggestedName.map((option) => option.label)}
+          sx={{width: '20%', maxWidth:'400px', minWidth:"80px",
+            '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline':{borderColor: "green"}}}
+  
+        
+        
+        
+        renderInput={(params) => <TextField {...params} label="Your username" />}
+ /></div>
+   <div className="flex items-center flex-nowrap gap-x-[10px]">
         <label htmlFor="password">Password: </label>
-        <TextField type="password" value={password} variant="outlined" label="your password" onChange={passhandeler} />
-        <Button variant="outlined" value="submit">
+        <TextField type="password" variant="outlined" label="your password" />
+</div>
+        <Button sx={{width:'80px', maxWidth:'20%'}} variant="outlined" value="submit">
           Submit
         </Button>
+        </div>
       </form>
     </div>
   );
